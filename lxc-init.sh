53ef86b0886cbc752591e592e21ccd16b40d6253
Damien BRIAND <da.briand04@gmail.com>
2025-11-23 17:11:48 +0100

    feat: fonctions utilitaire

--- lxc-init.sh
+++ lxc-init.sh
@@ -1,9 +1,38 @@
 #! /usr/bin/env bash
 
+c0=$(tput sgr0)
+myself=$(basename "$0")
+
 # Configuration par défaut
 distribution="debian"
 release="trixie"
 architecture="amd64"
+
+# Fonctions Utilitaire
+msg() {
+	[ -n "${2-}" ] && tput setaf "$2"
+	printf "%s%s\n" "$1" "$c0"
+}
+
+error() {
+	msg "Erreur: $1" 9
+	[ -z "${2-}" ] || exit "$2"
+}
+
+usage() {
+	cat <<- EOF
+		Usage: $myself -n NOM [-u USER] [-p PASS] [-d DISTRO] [-r RELEASE] [-a ARCH] [-h]
+		Où:
+		    -n, --name         : nom du conteneur (obligatoire)
+		    -u, --user         : nom de l'utilisateur à créer (défaut: user)
+		    -p, --password     : mot de passe de l'utilisateur (demandé si non fourni)
+		    -d, --distribution : distribution à installer (défaut: debian)
+		    -r, --release      : version de la distribution (défaut: trixie)
+		    -a, --architecture : architecture du conteneur (défaut: amd64)
+		    -h, --help         : affiche cette aide
+	EOF
+}
+
 
 # Gestion des options
 opts=$(getopt -o n:u:d:r:a:h --long name:,user:,distribution:,release:,architecture:,help -n "$myself" -- "$@") \
@@ -25,22 +54,22 @@ while true; do
 	esac
 done
 
-## Gestion des permissions
+# Gestion des permissions
 sudo -k
 sudo -v
 
 # Creation Conteneur
 sudo lxc-create -t download -n "$ct_name" -- -d "$distribution" -r "$release" -a "$architecture"
 
-echo "Demarrage du conteneur"
+msg "=== Démarrage du conteneur ===" 11
 sudo lxc-start -n "$ct_name"
 
-echo "Configuration du conteneur"
+msg "=== Configuration du conteneur  ===" 11
 sudo lxc-attach -n "$ct_name" -- bash -c "
 uname -a
 "
 
-echo "Arret et suppression"
+msg "=== Arret et suppression ===" 11
 sudo lxc-stop "$ct_name"
 sudo lxc-destroy "$ct_name"
 
